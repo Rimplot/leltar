3f9a40ce8299c9915aeed64acbcbdc41ce8dcae4
Kis Lázár Bence <akislb99@gmail.com>
2018-06-02 20:08:20 +0200

    Routing and initialization for categories

--- application/controllers/Categories.php
+++ application/controllers/Categories.php
@@ -2,11 +2,25 @@
 
 class Categories extends CI_Controller
 {
-    public function index() {
+    public function index()
+    {
         $data['page'] = 'categories';
         $data['page_title'] = "Kategóriák";
+        //$data['items'] = $this->categories_model->get_categories();
 
         $this->load->view('templates/header', $data);
+        $this->load->view($data['page'], $data);
+        $this->load->view('templates/footer');
+    }
+
+    public function view($id = false, $msg = null)
+    {
+        $data['page'] = 'category';
+        $data['page_title'] = "Kategória";
+        //$data['category'] = $this->categories_model->get_categories($id);
+
+        $this->load->view('templates/header', $data);
+        if ($msg == 'success') $this->load->view('success');
         $this->load->view($data['page'], $data);
         $this->load->view('templates/footer');
     }
