eab43b1a2cf5bae3a84b0db4b2109a46f706665a
Kis Lázár Bence <akislb99@gmail.com>
2020-02-18 11:51:59 +0100

    Items page – show only common properties #48

--- application/views/items/items.php
+++ application/views/items/items.php
@@ -10,14 +10,14 @@
 <table class="table">
     <thead>
         <tr>
-            <th scope="col">Azonosító</th>
-            <th scope="col">Név</th>
-            <th scope="col">Vonalkód</th>
+            <th scope="col" style="width: 10%">Azonosító</th>
+            <th scope="col" style="width: 40%">Név</th>
+            <!--<th scope="col">Vonalkód</th>-->
             <th scope="col">Kategória</th>
-            <th scope="col">Tulajdonos</th>
+            <!--<th scope="col">Tulajdonos</th>
             <th scope="col">Utoljára leltározva</th>
             <th scope="col">Session</th>
-            <th scope="col">Hely</th>
+            <th scope="col">Hely</th>-->
         </tr>
     </thead>
     <tbody>
@@ -31,12 +31,12 @@
                     <a href="<?php echo base_url() . 'items/' . $item['id']; ?>"><?php echo $item['name']; ?></a>
                     <?php if ($item['instance_count'] > 1) echo '<span class="badge badge-pill badge-warning float-right">' . $item['instance_count'] . ' példány</span>'; ?>
                 </td>
-                <td><?php echo $item['barcode']; ?></td>
+                <!--<td><?php echo $item['barcode']; ?></td>-->
                 <td><a href="<?php echo base_url() . 'categories/' . $item['category_id']; ?>"><?php echo $item['category']; ?></a></td>
-                <td><?php echo ($item['owner']) ? $item['owner'] : '<em>miénk</em>'; ?></td>
+                <!--<td><?php echo ($item['owner']) ? $item['owner'] : '<em>miénk</em>'; ?></td>
                 <td><?php echo ($item['last_seen']['time']) ? $item['last_seen']['time'] : '<em>ismeretlen</em>'; ?></td>
                 <td><?php echo ($item['last_seen']['session']) ? $item['last_seen']['session'] : '<em>ismeretlen</em>'; ?></td>
-                <td><?php echo ($item['last_seen']['storage_id']) ? '<a href="' . base_url() . 'storages/' . $item['last_seen']['storage_id'] . '">' . $item['last_seen']['storage'] . '</a>' : '<em>ismeretlen</em>'; ?></td>
+                <td><?php echo ($item['last_seen']['storage_id']) ? '<a href="' . base_url() . 'storages/' . $item['last_seen']['storage_id'] . '">' . $item['last_seen']['storage'] . '</a>' : '<em>ismeretlen</em>'; ?></td>-->
             </tr>
         <?php endforeach; ?>
     </tbody>
